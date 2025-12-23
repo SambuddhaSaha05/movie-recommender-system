54b654d292e0a0be3f12d8c95cbea5dee0718b1c
SambuddhaSaha05 <sambuddhasaha2023@gmail.com>
2025-12-24 02:33:44 +0530

    Fix drive download and data loading

--- app.py
+++ app.py
@@ -1,99 +1,70 @@
 import os
 import requests
+import pickle
+import pandas as pd
+import streamlit as st
 
+# ---------- Google Drive download ----------
 def download_from_drive(url, filename):
     if os.path.exists(filename):
         return
 
-    print(f"Downloading {filename}...")
+    st.write(f"Downloading {filename}...")
     response = requests.get(url, stream=True)
     with open(filename, "wb") as f:
         for chunk in response.iter_content(32768):
             if chunk:
                 f.write(chunk)
-    print(f"{filename} downloaded successfully.")
+    st.write(f"{filename} downloaded successfully.")
 
-    MOVIE_DICT_URL = "https://drive.google.com/uc?export=download&id=1oGCSRzQb9rQksmgPrm6WrCkhoHPhBc3P"
-    SIMILARITY_URL = "https://drive.google.com/uc?export=download&id=1-R_LVjqm4RPr-j1XT0U9PAnX7_JI1Pbx"
+MOVIE_DICT_URL = "https://drive.google.com/uc?export=download&id=1oGCSRzQb9rQksmgPrm6WrCkhoHPhBc3P"
+SIMILARITY_URL = "https://drive.google.com/uc?export=download&id=1-R_LVjqm4RPr-j1XT0U9PAnX7_JI1Pbx"
 
-    download_from_drive(MOVIE_DICT_URL, "movie_dict.pkl")
-    download_from_drive(SIMILARITY_URL, "similarity.pkl")
+download_from_drive(MOVIE_DICT_URL, "movie_dict.pkl")
+download_from_drive(SIMILARITY_URL, "similarity.pkl")
 
+# ---------- Load data ----------
 movies_dict = pickle.load(open("movie_dict.pkl", "rb"))
 movies = pd.DataFrame(movies_dict)
-
 similarity = pickle.load(open("similarity.pkl", "rb"))
 
-
-import streamlit as st
-import pickle
-import pandas as pd
-import requests
-
+# ---------- Recommendation logic ----------
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=6bcf4176dd5a400c1f5c569989ff31e6&language=en-US'.format(movie_id))
+    response = requests.get(
+        f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=6bcf4176dd5a400c1f5c569989ff31e6&language=en-US"
+    )
     data = response.json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-
+    return "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
 
 def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
+    movie_index = movies[movies["title"] == movie].index[0]
     distances = similarity[movie_index]
 
-    movies_list = sorted(
-        list(enumerate(distances)),
-        reverse=True,
-        key=lambda x: x[1]
-    )[1:6]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
     recommended_movies_posters = []
+
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch poster from API
         recommended_movies_posters.append(fetch_poster(movie_id))
 
     return recommended_movies, recommended_movies_posters
 
-
-
-movies_dict = pickle.load(open('movie_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
-
-similarity = pickle.load(open('similarity.pkl','rb'))
-
-
-st.title('Movie Recommender System')
+# ---------- Streamlit UI ----------
+st.title("Movie Recommender System")
 
 selected_movie_name = st.selectbox(
-    'How would you like to be contacted?',
-     movies['title'].values)
+    "Select a movie:",
+    movies["title"].values
+)
 
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie_name)
 
-
-if st.button('Recommend'):
-        names, posters = recommend(selected_movie_name)
-
-        col1, col2, col3, col4, col5 = st.columns(5)
-
-        with col1:
-            st.text(names[0])
-            st.image(posters[0])
-
-        with col2:
-            st.text(names[1])
-            st.image(posters[1])
-
-        with col3:
-            st.text(names[2])
-            st.image(posters[2])
-
-        with col4:
-            st.text(names[3])
-            st.image(posters[3])
-
-        with col5:
-            st.text(names[4])
-            st.image(posters[4])
-
+    cols = st.columns(5)
+    for col, name, poster in zip(cols, names, posters):
+        with col:
+            st.text(name)
+            st.image(poster)
